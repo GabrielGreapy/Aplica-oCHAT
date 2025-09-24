74c7700aa4be89f7ef92174b8b6d8ba04b431e9a
Gabriel Vinicius Santos Martins <gabrielmartinswebdev@gmail.com>
2025-09-24 10:46:35 -0300

    Firebase e Login melhorados

--- chat-g/Firebase/FirebaseConfig.tsx
+++ chat-g/Firebase/FirebaseConfig.tsx
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBKjBLmbBheTFZfKfaUyb-6A9ZVcZURznY",
@@ -15,5 +17,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
--- chat-g/app/components/button-login.tsx
+++ chat-g/app/components/button-login.tsx
@@ -1,6 +1,11 @@
 "use client"
 
-import { auth } from "@/Firebase/FirebaseConfig";
+import { auth, db } from "@/Firebase/FirebaseConfig";
+import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+
+
+
+
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import styles from "@/app/login/login.module.css"
@@ -12,6 +17,27 @@ export default function LoginButton(){
         
         try{
             const userCredential = await signInWithPopup(auth , provider);
+            const user = userCredential.user;
+
+            const userRef = doc( db, "users", user.uid);
+            const docSnap = await getDoc(userRef);
+
+            if( docSnap.exists()){
+                await updateDoc(userRef, {
+                    lastLogin : new Date(),
+                })
+            }
+            await setDoc( doc( db, "users", user.uid), {
+                uid : user.uid, 
+                name : user.displayName,
+                email : user.email,
+                avatarURL : user.photoURL,
+                createdAr : new Date(),
+                lastLogin : new Date(),
+            });
+
+
+
             const idToken = await userCredential.user.getIdToken();
             await fetch('/api/session', {
                 method:'POST',
