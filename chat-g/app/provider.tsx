e272178edb59cb22e671a06dce7f73a5d758399b
Gabriel Vinicius Santos Martins <gabrielmartinswebdev@gmail.com>
2025-12-06 15:36:57 -0300

    função de abrir chat adicionada

--- chat-g/app/provider.tsx
+++ chat-g/app/provider.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { AuthContextProvider } from "@/context/AuthContext";
-import { ChatInfoProvider } from "@/context/ChatInfoContext";
+
 import React from "react";
 
 export function Providers({ children} : {children : React.ReactNode}){
     return(
         <AuthContextProvider>
-        <ChatInfoProvider>
+     
             {children}
-        </ChatInfoProvider>
+       
         </AuthContextProvider>
     )
 }
